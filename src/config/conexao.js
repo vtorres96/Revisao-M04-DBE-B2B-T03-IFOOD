b4b3dde559531f9bc2cb2d8f152100f19933da35
Victor Torres Moreno <victor.torres_taking@totalexpress.com.br>
2023-03-30 15:03:40 -0300

    adicionando credenciais do banco de dados no .env

--- src/config/conexao.js
+++ src/config/conexao.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
+require('dotenv').config()
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'revisao_unidade_tres',
-    password: '03v01t96m',
-    port: 5432
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 
 const query = (text, param) => {
